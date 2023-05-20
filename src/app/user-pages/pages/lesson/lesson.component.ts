429223fcf998e909c745c1a10d55d25ea7ba1743
Hadi Kashlan <hadi.kashlan@akelius.de>
2023-05-20 18:36:11 +0200

    custom toolbar

--- src/app/user-pages/pages/lesson/lesson.component.ts
+++ src/app/user-pages/pages/lesson/lesson.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostBinding, inject } from '@angular/core';
+import { Component, ElementRef, HostBinding, Inject, inject } from '@angular/core';
 
+import { DOCUMENT } from '@angular/common';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { ExamComponent } from '../../../core/components/exam/exam.component';
 import { translationKeys } from '../../../core/models/translations';
@@ -25,7 +26,18 @@ export class LessonComponent {
   course = this.routeInfo.course;
 
   translationKeys = translationKeys;
+  elem: any;
 
+  constructor(@Inject(DOCUMENT) private document: any, public element: ElementRef<Element>) {}
+
+  toggleFullScreen() {
+    this.elem = this.element.nativeElement.querySelector('ngx-extended-pdf-viewer');
+    if (document.fullscreenElement !== null) {
+      this.closeFullscreen();
+    } else {
+      this.openFullscreen();
+    }
+  }
   finishLesson(finished: boolean) {
     this.studentService.finishLesson(this.routeInfo.course.id, this.routeInfo.lessonId, finished).subscribe();
   }
@@ -33,5 +45,36 @@ export class LessonComponent {
   finishExam(mark: number) {
     this.lesson.mark = mark;
     this.studentService.finishExam(this.routeInfo.course.id, this.routeInfo.lessonId, mark).subscribe();
+  }
+
+  openFullscreen() {
+    if (this.elem.requestFullscreen) {
+      this.elem.requestFullscreen();
+    } else if (this.elem.mozRequestFullScreen) {
+      /* Firefox */
+      this.elem.mozRequestFullScreen();
+    } else if (this.elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.elem.webkitRequestFullscreen();
+    } else if (this.elem.msRequestFullscreen) {
+      /* IE/Edge */
+      this.elem.msRequestFullscreen();
+    }
+  }
+
+  /* Close fullscreen */
+  closeFullscreen() {
+    if (this.document.exitFullscreen) {
+      this.document.exitFullscreen();
+    } else if (this.document.mozCancelFullScreen) {
+      /* Firefox */
+      this.document.mozCancelFullScreen();
+    } else if (this.document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.document.webkitExitFullscreen();
+    } else if (this.document.msExitFullscreen) {
+      /* IE/Edge */
+      this.document.msExitFullscreen();
+    }
   }
 }
