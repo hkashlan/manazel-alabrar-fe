17b0c9c0731c8b3d7cb9e06b4eb537c30267522e
Hadi Kashlan <hadi.kashlan@akelius.de>
2023-05-21 17:03:50 +0200

    fix authentication refresh

--- src/app/core/interceptors/auth-interceptor.service.ts
+++ src/app/core/interceptors/auth-interceptor.service.ts
@@ -1,5 +1,6 @@
 import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable()
@@ -18,6 +19,17 @@ export class AuthInterceptor implements HttpInterceptor {
       : req;
 
     // Pass the cloned request to the next handler
-    return next.handle(authReq);
+    return next.handle(authReq).pipe(
+      catchError((err) => {
+        if ([401, 403].includes(err.status)) {
+          // auto logout if 401 or 403 response returned from api
+          this.authService.logout();
+        }
+
+        const error = err.error?.message || err.statusText;
+        console.error(err);
+        return throwError(() => error);
+      })
+    );
   }
 }
